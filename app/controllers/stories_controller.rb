826a3a486357c877f14bbf75a057d962f9b89f87
br-silva <brunorafael6656@gmail.com>
2018-08-10 18:10:06 -0300

    Add ajax on stories page
    Controller actions and notifications handled by ajax and jquery

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,32 +1,26 @@
 class StoriesController < ApplicationController
-  before_action :set_story, only: %i[show edit update destroy]
+  before_action :set_story, only: %i[edit update destroy]
 
   def index
-    @stories = Story.all
-  end
-
-  def show; end
-
-  def new
+    @stories = Story.all.order(created_at: :desc)
     @story = Story.new
   end
 
-  def edit; end
+  def edit
+    respond_to :html, :js
+  end
 
   def create
     @story = Story.new(story_params)
-
     respond_to do |format|
       if @story.save
-        format.html do
-          redirect_to @story, notice: 'Story was successfully created.'
-        end
-        format.json { render :show, status: :created, location: @story }
+        flash[:success] = 'Story was successfully created.'
+
+        format.html { redirect_to stories_url }
+        format.js
       else
-        format.html { render :new }
-        format.json do
-          render json: @story.errors, status: :unprocessable_entity
-        end
+        format.html { render :index }
+        format.js { render_error_messages }
       end
     end
   end
@@ -34,26 +28,24 @@ class StoriesController < ApplicationController
   def update
     respond_to do |format|
       if @story.update(story_params)
-        format.html do
-          redirect_to @story, notice: 'Story was successfully updated.'
-        end
-        format.json { render :show, status: :ok, location: @story }
+        flash[:success] = 'Story was successfully updated.'
+
+        format.html { redirect_to stories_url }
+        format.js
       else
         format.html { render :edit }
-        format.json do
-          render json: @story.errors, status: :unprocessable_entity
-        end
+        format.js { render_error_messages }
       end
     end
   end
 
   def destroy
     @story.destroy
+    flash[:success] = 'Story was successfully destroyed.'
+
     respond_to do |format|
-      format.html do
-        redirect_to stories_url, notice: 'Story was successfully destroyed.'
-      end
-      format.json { head :no_content }
+      format.html { redirect_to stories_url }
+      format.js
     end
   end
 
@@ -65,5 +57,9 @@ class StoriesController < ApplicationController
 
   def story_params
     params.require(:story).permit(:title, :body)
+  end
+
+  def render_error_messages
+    render json: @story.errors.full_messages, status: :unprocessable_entity
   end
 end
